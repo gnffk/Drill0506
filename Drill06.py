cbeb91ed8cf6b9c2fd966a8d2825e9b53d000c30
minsu <gnffk48@naver.com>
2023-10-04 20:26:00 +0900

    Drill 06 개발 준비 기본 상태 완료. 소년이 가운데 IDLE 애니메이션.

--- Drill06.py
+++ Drill06.py
@@ -34,7 +34,7 @@ def reset_world():
     frame = 0
     action = 3
 
-    set_name_target_arrow()
+    #set_name_target_arrow()
 
 
 def set_name_target_arrow():
@@ -50,7 +50,7 @@ def set_name_target_arrow():
 def render_world():
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    arrow.draw(hx, hy)
+    #arrow.draw(hx, hy)
     character.clip_draw(frame * 100, 100 * action, 100, 100, cx, cy)
     update_canvas()
 
@@ -63,13 +63,13 @@ def update_world():
     frame = (frame + 1) % 8
 
 
-    if t <= 1.0:
-        cx = (1 - t) * sx + t * hx
-        cy = (1 - t) * sy + t * hy
-        t += 0.001
-    else:
-        cx,xy = hx,hy
-        set_name_target_arrow()
+    #if t <= 1.0:
+    #    cx = (1 - t) * sx + t * hx
+    #    cy = (1 - t) * sy + t * hy
+    #    t += 0.001
+    #else:
+    #    cx,xy = hx,hy
+    #    set_name_target_arrow()
 
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 hide_cursor()
